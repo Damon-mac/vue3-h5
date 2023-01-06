b1d85355ef7c285bd56774cdb30301b27da80223
朱伟栋 <weidongzhu36@126.com>
2023-01-06 15:57:28 +0800

    fix(custom): 测试pretier

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,11 +7,18 @@ module.exports = {
     'plugin:vue/vue3-essential',
     '@vue/airbnb',
     '@vue/typescript/recommended',
+    'plugin:prettier/recommended', // 添加 prettier 插件,需要安装eslint-plugin-prettier, 这行必须加, 不然不会报错
     'prettier', // 这里注意需要安装eslint-config-prettier才能生效
   ],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2020, // 解析器
   },
+  /**
+   * 错误级别分为三种：
+   * "off" 或 0 - 关闭规则
+   * "warn" 或 1 - 开启规则，使用警告级别的错误：warn (不会导致程序退出)
+   * "error" 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
+   */
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
